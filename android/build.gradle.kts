9fdd7a0b32bef2932376fce1abbe829fb1e3c863
MahmoudAladwy <mahmoudaladwy774@gmail.com>
2025-03-05 17:27:49 +0200

    Fastlane & Gemfile

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,19 @@
+buildscript {
+    val kotlinVersion = "1.7.10"
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.0")
+        // START: FlutterFire Configuration
+        classpath("com.google.gms:google-services:4.3.10")
+        // END: FlutterFire Configuration
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -5,14 +21,14 @@ allprojects {
     }
 }
 
-rootProject.buildDir = file("build")
+rootProject.buildDir = File("../build")
 subprojects {
-    buildDir = file("build")
+    project.buildDir = File(rootProject.buildDir, project.name)
 }
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+    delete(rootProject.buildDir)
 }
