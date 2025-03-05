e30385d6a712da09e7dcbfa83c3cd49d412a5386
MahmoudAladwy <mahmoudaladwy774@gmail.com>
2025-03-05 15:40:28 +0200

    - fix gradle

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,12 +5,9 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
+rootProject.buildDir = file("build")
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    buildDir = file("build")
 }
 subprojects {
     project.evaluationDependsOn(":app")
