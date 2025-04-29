7729f68e8f536e18a057ceb91f44e0c0b2a2ce72
MahmoudAladwy <mahmoudaladwy774@gmail.com>
2025-04-29 04:00:41 +0300

    - delete stars from book appointment

--- lib/features/book_appointment/ui/widgets/book_appointment_header.dart
+++ lib/features/book_appointment/ui/widgets/book_appointment_header.dart
@@ -6,8 +6,8 @@ import '../../../../core/theme/text_styless.dart';
 import '../../../home/data/models/service_card_model.dart';
 
 class BookAppointmentHeader extends StatelessWidget {
-  const BookAppointmentHeader({super.key,required this.serviceCardModel});
-final ServiceCardModel serviceCardModel;
+  const BookAppointmentHeader({super.key, required this.serviceCardModel});
+  final ServiceCardModel serviceCardModel;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -28,17 +28,6 @@ final ServiceCardModel serviceCardModel;
             style: TextStyles.font18DarkBlueSemiBold.copyWith(
               letterSpacing: 0.9,
             ),
-          ),
-          Spacer(),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Icon(Icons.star, color: Colors.amber, size: 20),
-              Icon(Icons.star, color: Colors.amber, size: 20),
-              Icon(Icons.star, color: Colors.amber, size: 20),
-              Icon(Icons.star, color: Colors.amber, size: 20),
-              Icon(Icons.star_half, color: Colors.amber, size: 20),
-            ],
           ),
         ],
       ),
